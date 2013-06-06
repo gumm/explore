4ee08be709797d6add4ab7136e0628432caf4935
gumm <janhendrik.badenhorst@gmail.com>
2013-06-07 00:14:32 +0200

    Playing with layouts

--- public/js/app/site.js
+++ public/js/app/site.js
@@ -41,10 +41,13 @@ app.Site.prototype.initSite = function() {
 app.Site.prototype.initLayout_ = function() {
 
     var id = Math.floor(Math.random() * 2147483648).toString(36);
-    var mainCells = ['left', 'center', 'right'];
-    var innerCells = ['top', 'mid', 'bot'];
-    var topMargin = 90;
-    var bottomMargin = 20;
+    var mainCells = ['header', 'main', 'footer'];
+    var innerCellsHorizontal = ['left', 'center', 'right'];
+    var innerCellsVertical = ['top', 'mid', 'bottom'];
+    var topMargin = 0;
+    var rightMargin = 0;
+    var bottomMargin = 0;
+    var leftMargin = 0;
 
     /**
      * Create a new layout
@@ -52,46 +55,50 @@ app.Site.prototype.initLayout_ = function() {
      * @private
      */
     this.layout_ = new bad.ui.Layout(id, mainCells,
-        bad.ui.Layout.Orientation.HORIZONTAL
+        bad.ui.Layout.Orientation.VERTICAL
     );
 
     // Set the defaults for the site.
     this.layout_.setTarget(goog.dom.getDocument().body);
-    this.layout_.setInitialSize(mainCells[0], 260);
-    this.layout_.setInitialSize(mainCells[2], 200);
-    this.layout_.setDraggerThickness(5);
+    this.layout_.setInitialSize(mainCells[0], 41);
+    this.layout_.setInitialSize(mainCells[2], 23);
+    this.layout_.setDraggerThickness(0);
     this.layout_.setWidthToViewport(true);
     this.layout_.setHeightToViewport(true);
-    this.layout_.setMargin(topMargin, 0, bottomMargin, 0);
-    this.layout_.setMinimumSize(mainCells[0], 0);
-    this.layout_.setMinimumSize(mainCells[2], 0);
+    this.layout_.setMargin(topMargin, rightMargin, bottomMargin, leftMargin);
 
-    // Create a west layout.
-    this.layout_.setInnerLayout(
-        innerCells,
-        mainCells[0],
-        bad.ui.Layout.Orientation.VERTICAL
-    );
-
-    // Create an middle layout
-    this.layout_.setInnerLayout(innerCells,
+    // Create main horizontal layout.
+    var mainHorizontalLayout = this.layout_.setInnerLayout(
+        innerCellsHorizontal,
         mainCells[1],
-        bad.ui.Layout.Orientation.VERTICAL
+        bad.ui.Layout.Orientation.HORIZONTAL
     );
+    mainHorizontalLayout.setDraggerThickness(5);
+    mainHorizontalLayout.setInitialSize(innerCellsHorizontal[0], 0 - 5);
+    mainHorizontalLayout.setInitialSize(innerCellsHorizontal[2], 0 - 5);
 
-    // Create an east layout
-    this.layout_.setInnerLayout(
-        innerCells,
-        mainCells[2],
+    // Up-Down Layout in the left.
+    var leftVerticalLayout = mainHorizontalLayout.setInnerLayout(innerCellsVertical,
+        innerCellsHorizontal[0],
         bad.ui.Layout.Orientation.VERTICAL
     );
+    leftVerticalLayout.setInitialSize(innerCellsVertical[0], 0 - 5);
+    leftVerticalLayout.setInitialSize(innerCellsVertical[2], 0 - 5);
+
+    // Up-Down Layout in the right.
+    var rightVerticalLayout = mainHorizontalLayout.setInnerLayout(innerCellsVertical,
+        innerCellsHorizontal[2],
+        bad.ui.Layout.Orientation.VERTICAL
+    );
+    rightVerticalLayout.setInitialSize(innerCellsVertical[0], 0 - 5);
+    rightVerticalLayout.setInitialSize(innerCellsVertical[2], 0 - 5);
 
     this.listen(
         this.layout_,
         bad.ui.Layout.EventType.LAYOUT_READY,
         function(e) {
             if (e.target.getId() === id) {
-                console.debug('ALL LAYOUTS ARE READY AND IN THE DOM!');
+                this.initNavigation();
             }
         }
     );
@@ -104,10 +111,33 @@ app.Site.prototype.getLayout = function() {
     return this.layout_;
 };
 
+app.Site.prototype.initNavigation = function() {
+    this.hideAllNests();
+    this.slideOpenAllNests();
+};
+
+app.Site.prototype.hideAllNests = function() {
+    goog.object.forEach(this.getLayout().getNests(), function(nest) {
+        this.hideNest(nest);
+    }, this);
+};
+
 app.Site.prototype.slideHideAllNests = function() {
     goog.object.forEach(this.getLayout().getNests(), function(nest) {
         this.slideHideNest(nest);
     }, this);
+};
+
+app.Site.prototype.slideOpenAllNests = function() {
+    goog.object.forEach(this.getLayout().getNests(), function(nest) {
+        this.slideOpenNest(nest);
+    }, this);
+};
+
+app.Site.prototype.slideOpenNest = function(nest) {
+    if (nest.slideOpen) {
+        nest.slideOpen();
+    }
 };
 
 app.Site.prototype.slideHideNest = function(nest) {
@@ -118,20 +148,28 @@ app.Site.prototype.slideHideNest = function(nest) {
     }
 };
 
-app.Site.prototype.openLeft = function() {
-    this.getLayout().getNest('left').slideOpen();
+app.Site.prototype.hideNest = function(nest) {
+    if (nest.hide) {
+        nest.hide();
+    }
 };
 
-app.Site.prototype.openRight = function() {
-    this.getLayout().getNest('right').slideOpen();
+app.Site.prototype.openLeft = function(op_perCent, opt_pixels, opt_callback) {
+    this.getLayout().getNest('main', 'left').slideOpen(
+        op_perCent, opt_pixels, opt_callback);
+};
+
+app.Site.prototype.openRight = function(op_perCent, opt_pixels, opt_callback) {
+    this.getLayout().getNest('main', 'right').slideOpen(
+        op_perCent, opt_pixels, opt_callback);
 };
 
 app.Site.prototype.closeLeft = function() {
-    this.slideHideNest(this.getLayout().getNest('left'));
+    this.slideHideNest(this.getLayout().getNest('main', 'left'));
 };
 
 app.Site.prototype.closeRight = function() {
-    this.slideHideNest(this.getLayout().getNest('right'));
+    this.slideHideNest(this.getLayout().getNest('main', 'right'));
 };
 
 app.Site.prototype.openLeftToRight = function() {
